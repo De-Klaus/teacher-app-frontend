584992a51b42420922a0de321e49c719ad6a0e15
petko_isands <petko@isands.ru>
2025-03-01 01:24:38 +0700

    ret fix

--- src/pages/CreateStudentPage.jsx
+++ src/pages/CreateStudentPage.jsx
@@ -28,7 +28,7 @@ const CreateStudentPage = () => {
         const token = localStorage.getItem("token"); // Получаем токен
 
         try {
-            const response = await fetch(`${API_URL}/students/create`, {
+            const response = await fetch(`${API_URL}/students`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
